f2fe1844644583b14d4ae282e6ee07681c4c1eec
jrmoore117 <jrmoore117@gmail.com>
2022-01-27 18:55:02 -0800

    Add exmaple use of context provider.

--- src/components/PokedexFrame/index.js
+++ src/components/PokedexFrame/index.js
@@ -1,23 +1,39 @@
-import PropTypes from 'prop-types';
+import {
+  useEffect,
+  useState,
+} from 'react';
+import axios from 'axios';
 import classNames from 'classnames';
-import { PokemonProvider, usePokemonContext } from '../../contexts/PokemonContext';
+import PropTypes from 'prop-types';
+
+import { PokemonProvider } from '../../contexts/PokemonContext';
 
 export const PokedexFrame = ({
    children,
    className,
    ...props
 }) => {
+   
+   const [pokemon, setPokemon] = useState({});
+   const [pokemonNumber, setPokemonNumber ] = useState(1);
+   
+   // Make API Call
+   useEffect(() => {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`)
+         .then(res => {
+            // Set API Response to state
+            setPokemon(res.data);
+         })
+         .catch(err => console.log(err));
+   }, [pokemonNumber]);
+
    const pokedexFrameClasses = classNames(
       'pokedex-frame',
       className
    );
 
-   // Make API Call
-   // Set API Response to PokemonProvider value
-   // Access API response data through useContext anywhere else in the app
-   
    return (
-      <PokemonProvider value={null}>
+      <PokemonProvider value={pokemon}>
          <div
             className={pokedexFrameClasses}
             {...props}
--- src/components/PokedexFrameLeft/index.js
+++ src/components/PokedexFrameLeft/index.js
@@ -1,15 +1,22 @@
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+import { usePokemonContext } from '../../contexts/PokemonContext';
 
 export const PokedexFrameLeft = ({
    children,
    className,
    ...props
 }) => {
+   const pokemon = usePokemonContext();
+
    const pokedexFrameLeftClasses = classNames(
       'pokedex-frame-left',
       className
    );
+
+   console.log("pokemon from context", pokemon);
+   
    return (
       <div
          className={pokedexFrameLeftClasses}
